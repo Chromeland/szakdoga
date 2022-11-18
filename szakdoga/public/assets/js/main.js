6ff3dac0965a2ca8963a9871ff97b69ba0c9db5f
Chromeland <tozserbalex@outlook.com>
2022-11-18 01:49:27 +0100

    Modifier small work. Background "background" type submit.

--- szakdoga/public/assets/js/main.js
+++ szakdoga/public/assets/js/main.js
@@ -68,14 +68,35 @@ function modifierConst(elem) {
     containerDiv.setAttribute("id", "selected");
     containerDiv.classList.add("modifierFieldContainer");
 
-    let label = document.createElement("h3")
+    let label = document.createElement("h3");
     label.setAttribute("id", elem.id + "#mody");
     label.setAttribute("class", "SelectedElement");
     if (elem.id.includes("label")) {
         label.innerText = "Label Settings:";
+        containerDiv.appendChild(label);
     } else if (elem.id.includes("Background")) {
-        label.innerText = "Background Settings:";
+        label.innerText = "Background Settings";
+        containerDiv.appendChild(label);
+
+        let background_color = document.createElement("h4");
+        background_color.innerHTML = "Background Color:";
+        background_color.setAttribute("class","mody_label");
+
+        let background_color_input = document.createElement("input");
+        background_color_input.setAttribute("type","color");
+        background_color_input.setAttribute("id","background_color");
+        containerDiv.appendChild(background_color);
+        containerDiv.appendChild(background_color_input);
+
+        let background_image = document.createElement("h4");
+        background_image.innerHTML = "Background Image:";
+        background_image.setAttribute("class","mody_label");
+
+        let background_image_input = document.createElement("input");
+        background_image_input.setAttribute("type","file");
+        background_image_input.setAttribute("id","background_image");
+        containerDiv.appendChild(background_image);
+        containerDiv.appendChild(background_image_input);
     }
-    containerDiv.appendChild(label);
     modifier.appendChild(containerDiv);
 }
