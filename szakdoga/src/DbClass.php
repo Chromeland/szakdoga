bcc86eef149c278f741c296559b3f445933ba968
Chromeland <tozserbalex@outlook.com>
2022-11-21 21:07:20 +0100

    Text elements to db is now working smoothly. Border details seems to be good.

---
+++ szakdoga/src/DbClass.php
@@ -0,0 +1,57 @@
+<?php
+
+class DbClass
+{
+    private static $instance;
+
+    public static function Instance(): DbClass {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+
+    function saveTextToDB($id,$Type,$parentElement,$posX,$posY,$innerText,$Style = 'normal',$fontFamily = 'Arial',$fontColour = 'black',$fontSize = 12,$textFloat = 'left',$opacity = 1,$borderRadius = null,$borderStyle = null,$borderSize = null,$borderColor = null)
+    {
+        $tmp = explode("#", $id);
+        $id = $tmp[0];
+
+        $servername = "localhost:3306";
+        $username = "BenceTeszt";
+        $password = "123";
+        $dbname = "szakdolgozat";
+
+// Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $checkIfExists = $conn->query("SELECT * FROM `texts` WHERE `ID` = '" . $id . "'");
+        //$result = $checkIfExists->fetch_array(MYSQLI_ASSOC);
+        $rowCount = mysqli_num_rows($checkIfExists);
+//checking if the element already exists
+        if ($rowCount < 1) {
+            $add = "INSERT INTO `texts` VALUES ('" . $id . "','" . $Type . "','" . $parentElement . "','" . $posX . "','" . $posY . "','" . $innerText . "','" . $Style . "','" . $fontFamily . "','" . $fontColour . "','" . $fontSize . "','" . $textFloat . "','" . $opacity . "'";
+            if($borderColor){
+                $add .= ",'" . $borderRadius . "','" . $borderStyle . "','" . $borderSize . "','" . $borderColor . "')";
+                $conn->query($add);
+                echo "New record created successfully";
+            }else{
+                $add .= ",NULL,NULL,NULL,NULL)";
+                $conn->query($add);
+                echo "New record created successfully";
+            }
+
+        } else {
+            $update = "UPDATE `texts` SET `ID`= '$id',`Type`='$Type',`ParentElement`='$parentElement',`Position-x`='$posX',`Position-y`='$posY',`InnerText`='$innerText',`Style`='$Style',`FontFamily`='$fontFamily',`FontColor`='$fontColour',`FontSize`='$fontSize',`Text-Float`='$textFloat',`Opacity`='$opacity',`Border-Radius`='$borderRadius',`Border-Style`='$borderStyle',`Border-Size`='$borderSize',`Border-Color`='$borderColor' WHERE `ID` = '$id'";
+            $conn->query($update);
+            echo "Record update successful";
+        }
+        $conn->close();
+        return true;
+    }
+}
--- szakdoga/src/PrepareClass.php
+++ szakdoga/src/PrepareClass.php
@@ -1,39 +1,9 @@
 <?php
 
+include 'DbClass.php';
+
 if ($_POST["type"] === 'TextToDb') {
-    echo(json_encode(saveTextToDB($_POST["ID"],$_POST["Type"],$_POST["parentElement"],$_POST["posX"],$_POST["posY"],$_POST['innerText'],$_POST["style"],$_POST["fontFamily"],$_POST["fontColor"],$_POST["fontSize"],$_POST["textFloat"],$_POST["opacity"],$_POST["borderRadius"],$_POST["borderStyle"],$_POST["borderSize"],$_POST['borderColor'])));
+    echo(json_encode(DbClass::Instance()->saveTextToDB($_POST["ID"],$_POST["Type"],$_POST["parentElement"],$_POST["posX"],$_POST["posY"],$_POST['innerText'],$_POST["style"],$_POST["fontFamily"],$_POST["fontColor"],$_POST["fontSize"],$_POST["textFloat"],$_POST["opacity"],$_POST["borderRadius"],$_POST["borderStyle"],$_POST["borderSize"],$_POST['borderColor'])));
 } else {
-    echo(json_encode($_POST["ID"]));
-}
-
-function saveTextToDB($id,$Type,$parentElement,$posX,$posY,$innerText,$Style = 'normal',$fontFamily = 'Arial',$fontColour = 'black',$fontSize = 12,$textFloat = 'left',$opacity = 1,$borderRadius = null,$borderStyle = null,$borderSize = null,$borderColor = null)
-{
-    $tmp = explode("#", $id);
-    $id = $tmp[0];
-
-    $servername = "localhost:3306";
-    $username = "BenceTeszt";
-    $password = "123";
-    $dbname = "szakdolgozat";
-
-// Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $checkIfExists = $conn->query("SELECT * FROM `texts` WHERE `ID` = '" . $id . "'");
-    $rowCount = mysqli_num_rows($checkIfExists);
-    if ($rowCount < 1) {
-        $add = "INSERT INTO `texts` VALUES ('" . $id . "','" . $Type . "','" . $parentElement . "','" . $posX . "','" . $posY . "','" . $innerText . "','" . $Style . "','" . $fontFamily . "','" . $fontColour . "','" . $fontSize . "','" . $textFloat . "','" . $opacity . "','" . $borderRadius . "','" . $borderStyle . "','" . $borderSize . "','" . $borderColor . "')";
-        $conn->query($add);
-        echo "New record created successfully";
-    } else {
-        $update = "UPDATE `texts` SET `ID`= '$id',`Type`='$Type',`ParentElement`='$parentElement',`Position-x`='$posX',`Position-y`='$posY',`InnerText`='$innerText',`Style`='$Style',`FontFamily`='$fontFamily',`FontColor`='$fontColour',`FontSize`='$fontSize',`Text-Float`='$textFloat',`Opacity`='$opacity',`Border-Radius`='$borderRadius',`Border-Style`='$borderStyle',`Border-Size`='$borderSize',`Border-Color`='$borderColor' WHERE `ID` = '$id'";
-        $conn->query($update);
-        echo "Record update successful";
-    }
-    $conn->close();
-    return true;
+    echo("Something went wrong!");
 }
